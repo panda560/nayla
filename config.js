1229c2727ea1a3c7cb312aa89548173b72655029
Muhammad Arrezza Faiz Pratama <103143006+panda560@users.noreply.github.com>
2022-06-24 09:41:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,28 +3,28 @@ let chalk = require('chalk')
 
 global.Owner = {
     // use the phone number with the country code, for example Indonesia '62' America '1' etc
-    '628818770766': { // 628818770766
-        name: 'Zevano',
+    '6281273677810': { // 6281273677810
+        name: 'Bianofc',
         isReport: true, // if true this number will send if user use command /report or /join
         isCreator: true, // if true this number will send if user use command /owner or /creator
         isDev: true
     }
 }
-global.mods = ['6283897390164'] // Want some help?
+global.mods = ['6281273677810'] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Premium user has unlimited limit
 global.DeveloperMode = 'false' // true or false
-global.linkGC = ['https://chat.whatsapp.com/JgFOfu182li7kG6dlKcwzD']
-global.packname = 'NayLa-B0TZ'
-global.author = 'Zevano'
+global.linkGC = ['https://chat.whatsapp.com/KrYTgYfS4YE9vCZNGmrLcQ']
+global.packname = 'BiAn-B0TZ'
+global.author = 'Bianofc'
 global.multiplier = 36 // The higher, The harder levelup
 global.sessionName = 'nayla' // The Session name behind the .json example is nayla.json
 global.mesreport = 50
-global.botname = 'NAYLA-BOTZ'
+global.botname = 'BiAn-BOTZ'
 global.thumnail = ext.thum // It's hard to change if you don't get an error
 global.thum = ext.thum // Same as above
 global.ext = {
 	title: 'Sabar sedang di proses',
-	body: 'NayLa-B0TZ',
+	body: 'BiAn-B0TZ',
 	thum: 'https://telegra.ph/file/5859353a380d9b67be198.jpg'
 }
     global.mess = {
@@ -38,8 +38,8 @@ global.ext = {
 	admin: 'Maaf fitur ini hanya bisa digunakan *Admin*',
     botAdmin: 'Jadikan bot sebagai *Admin* untuk menggunakan fitur ini',
     unreg: '── 「 NOT REGISTERED 」 ──\nSilakan Register Terlebih Dahulu Sebelum Menggunakan Bot. Cara Register Cukup Dengan Command *.daftar nama.umur*\nContoh .daftar ' + global.botname + '.16\n\nNote\nHarap Save Serial Number Mu Agar Bisa Melakukan Unreg Database Bot',
-    sukses: 'Done Ya kawand',
-    wm: '© nayla',
+    sukses: 'Done Tod',
+    wm: '© bian',
     wel: 'Hai @user\n◪ Welcome in group:\n├─ @subject\n├─ Intro dulu\n├─ ❏ Nama: \n├─ ❏ Umur: \n├─ ❏ Asal kota: \n├─ ❏ Kelas: \n├─ ❏ Jenis kelamin:',
     bye: 'Selamat Tinggal @user!\nTerimakasih Telah Bergabung Di Grup\n\nKalo Balik Jangan Lupa Bawa Gorengan Buat Anggota Disini',
     promote: '「 PROMOTE - DETECTED 」\nTarget : @user\nStatus : Member -> Admin\nSELAMAT KAMU JADI ADMIN',
